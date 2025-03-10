d399b7224bee4c685d10a3babdc06118964b99f9
Marco Zamparelli <marco.zamparelli@libero.it>
2025-03-10 18:14:00 +0100

    Resize

---
+++ resources/js/Pages/Carta.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let num;
+    export let height="100px";
+    export let isFaceDown=false;
+
+    let imgUrl;
+
+    if(isFaceDown)
+        imgUrl="/images/dorso.jpg"
+    else
+        imgUrl="/images/"+num+".jpg"
+    
+
+</script>
+
+
+
+
+<div style="height:{height};">
+
+    <img style='height:100%' src={imgUrl} alt="carta"/>
+
+</div>
--- resources/js/Pages/Index.svelte
+++ resources/js/Pages/Index.svelte
@@ -1,16 +1,45 @@
 <script>
+    import { onMount, onDestroy } from "svelte";
+    import ResizeObserver from "svelte-resize-observer";
+    import Carta from "./Carta.svelte";
+
     export let title;
     export let errors;
+
+    let cardHeight;
+    let mainReference;
+
+    onMount(() => {
+      //  let tmp = Math.round((window.innerHeight / 100) * 35);
+       // cardHeight = tmp + "px";
+    });
+
+    onDestroy(() => {
+        console.log("Component unmounted!");
+    });
+
+    function onResize({ detail }) {
+      //  width = detail.width;
+      //  height = detail.height;
+
+        let tmp = Math.round((window.innerHeight / 100) * 35);
+        cardHeight = tmp + "px";
+    }
 </script>
 
+<ResizeObserver elementResize={mainReference} on:resize={onResize} />
 
-<main>
-    <div class="flex flex-col items-center text-center space-y-4">
-        <h1 class="block mt-4 font-bold text-4xl">{title}</h1>
-        <img src="/images/dorso.jpg" alt="" />
+<main bind:this={mainReference}>
+    <h1 class="block mt-4 font-bold text-4xl">{title}</h1>
+    <div class="d-flex flex-row justify-content-evenly">
+        <Carta num="5" isFaceDown="true" height={cardHeight} />
+        <Carta num="2" height={cardHeight} />
+        <Carta num="7" height={cardHeight} />
     </div>
-    
-
 </main>
 
-
+<style>
+    h1 {
+        color: red;
+    }
+</style>
